a7c5864f804a5d1b84be917528943bcfbc1b442d
annayqho <annayqho@gmail.com>
2018-05-07 17:33:24 -0700

    add a py init to see if you can import from subdir

---
+++ search.py
@@ -0,0 +1,29 @@
+""" 
+This script takes in an RA, Dec, and Time.
+It searches the GBM history to see whether the detector was pointing that way
+at the time. 
+"""
+
+import numpy as np
+from astropy.time import Time
+from gbm.GBMgeo import GBMgeo
+
+
+def search_gbm(ra, dec, t):
+    """ Search the GBM history to see whether the detector was
+    sensitive to a given position at a given time
+    
+    Parameters
+    ----------
+    ra: right ascension in degrees
+    dec: declination in degrees
+    t: time in astropy isot format
+    """
+
+
+if __name__=="__main__":
+    ra = 221.491713
+    dec = 14.993165
+    # burst time of iPTF14yb
+    t = Time('2014-02-26T10:02:57', format='isot', scale='utc')
+
