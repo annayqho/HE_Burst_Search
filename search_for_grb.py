a29c301105a67f56f83eb1dc81becb33265f9f61
annayqho <annayqho@gmail.com>
2019-09-10 14:21:55 -0700

    actually check IPN burst times

--- search_for_grb.py
+++ search_for_grb.py
@@ -8,14 +8,17 @@ import requests
 import re
 import subprocess
 import os
+from time import strptime
 from astropy.time import Time
+from astropy.coordinates import SkyCoord
+from astropy import units as u
 
 window_size = 2
 
 # Koala search parameters, for testing
 ra = 30.063307 
 dec = 16.799255
-erad = 1 # degree
+c = SkyCoord(ra, dec, unit='deg')
 time = Time(2458373.9075, format="jd")
 window = Time(
         np.linspace(time.jd-window_size, time.jd, window_size), format='jd')
@@ -24,6 +27,8 @@ window = Time(
 
 # CHECK IPN
 # Note: catalog ends 30 June 2019
+
+print("CONDUCTING SEARCH OF IPN CATALOG")
 
 def get_searchstr(t):
     yy = str(t.datetime.year)[2:]
@@ -42,10 +47,25 @@ if os.path.exists(fname) is False:
 lines = np.array(open(fname, "r").readlines())
 header = lines[np.array([' DOY TIME ' in l for l in lines])][0]
 keep = np.array([l[7:16] in searchstr for l in lines])
-print("There are %s bursts in the last %s days" %(sum(keep), window_size))
+
+# Now, check each one to see if the time is correct
+final_set = []
+for l in lines[keep]:
+    filtered_l = [i for i in l.split(" ") if i]
+    burst_dd = filtered_l[0].split('.')[1]
+    burst_mm = str(strptime(filtered_l[1], '%b').tm_mon).zfill(2)
+    burst_yy = str(filtered_l[2]).zfill(2)
+    burst_time = filtered_l[4]
+    burst_datetime = Time(
+            '20%s-%s-%sT%s' %(burst_yy,burst_mm,burst_dd,burst_time), 
+            format='isot')
+    if np.logical_and(burst_datetime > window[0], burst_datetime < window[-1]):
+        final_set.append(l)
+
+print("There are %s bursts in the last %s days" %(len(final_set), window_size))
 
 # Check which spacecraft observed these bursts
-for l in lines[keep]:
+for l in final_set:
     det_by = np.array(
             [header[i.start():i.end()] for i in re.finditer('YES', l)])
     print(det_by)
@@ -53,12 +73,15 @@ for l in lines[keep]:
 ######################################
 
 # CHECK FERMI
+
+print("CONDUCTING SEARCH OF FERMI CATALOG")
+
 www = 'https://heasarc.gsfc.nasa.gov/db-perl/W3Browse/w3query.pl'
 data = {}
 data['tablehead'] = 'name=heasarc_fermigbrst&description=Fermi GBM Burst Catalog&url=http://heasarc.gsfc.nasa.gov/W3Browse/fermi/fermigbrst.html&archive=Y&radius=180&mission=FERMI&priority=1&tabletype=Object'
 data['Time'] = '%s .. %s' %(window[0].mjd, window[-1].mjd)
 data['displaymode'] = 'PureTextDisplay'
-data['varon'] = 'error_radius'
+data['varon'] = 'name,ra,dec,trigger_time,error_radius'
 r = requests.post(url = www, data = data)
 out = np.array([i for i in r.text.split('\n') if i])
 header = [i for i in out[2].split('|')]
@@ -68,13 +91,19 @@ if ncands == 0:
 else:
     print("Found %s in Fermi" %ncands)
     for ii in np.arange(ncands):
-        cands = out[ii]
-        vals = [i for i in out[3+ii].split('|')]
+        cand = out[3+ii]
+        vals = [i for i in cand.split('|')]
+        print(vals)
         grbname = vals[1]
         grbra = vals[2]
         grbdec = vals[3]
         grbtime = vals[4]
-        print("%s with RA=%s, Dec=%s on t=%s" %(grbname,grbra,grbdec,grbtime))
+        grbepos = vals[5].strip()
+        print("%s with RA=%s, Dec=%s on t=%s with %s deg uncertainty"%(
+            grbname,grbra,grbdec,grbtime,grbepos))
+        c2 = SkyCoord('%s %s' %(grbra,grbdec), unit=(u.hourangle, u.deg))
+        dist = c2.separation(c).degree
+        print("The burst is %s deg away from the source" %dist)
 
 # subthreshold notices: 
 # https://gcn.gsfc.nasa.gov/fermi_gbm_subthresh_archive.html
