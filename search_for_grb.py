40ab20c019b1ef288ca13d1a400e5dbb1f3ab76d
annayqho <annayqho@gmail.com>
2019-09-09 20:41:32 -0700

    download ipn file

--- search_for_grb.py
+++ search_for_grb.py
@@ -3,6 +3,8 @@ Given a specific position and time and search radius,
 when is the last time a GRB happened in this location?
 """
 
+import subprocess
+import os
 from astropy.time import Time
 
 # Search parameters, for testing
@@ -11,5 +13,19 @@ dec = 61.497984
 erad = 1 # degree
 time = Time(2458728.6798, format="jd")
 
+######################################
+
+# CHECK IPN
+
+# You need the day, month, year
 
 
+fpath = "http://www.ssl.berkeley.edu/ipn3/masterli.txt"
+fname = fpath.split('/')[-1]
+if os.path.exists(fname) is False:
+    subprocess.call(['wget', 'http://www.ssl.berkeley.edu/ipn3/masterli.txt'])
+for line in open(fname, "r"):
+    dat = line.split(' ')
+    dat = [i for i in dat if i]
+    print(dat)
+    print(len(dat))
